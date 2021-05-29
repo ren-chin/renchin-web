3f722fab754ee0dd4da1b518a50c171dc7d40055
soe-j <7115203+soe-j@users.noreply.github.com>
2021-05-30 00:34:37 +0900

    feat: to select wattage

---
+++ src/components/atoms/WattageSelect.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wattage-select">
+    <span class="wattage-description">使うレンジ</span>
+    <div class="wattage-container">
+      <div v-for="item in list" :key="item.id" class="wattage-item">
+        {{ item.name }}
+      </div>
+    </div>
+    <span class="wattage-unit">W</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      list: [
+        1500, 1400, 1300, 1200, 1000, 900, 800, 700, 600, 500, 400, 300, 200,
+      ].map((wattage) => {
+        return {
+          id: wattage,
+          name: `${wattage}`,
+        };
+      }),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.wattage-select {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ea5541;
+}
+.wattage-description {
+  font-size: 22px;
+  margin-right: 8px;
+  height: 22px;
+}
+.wattage-unit {
+  font-size: 26px;
+  margin-left: 4px;
+  height: 30px;
+}
+.wattage-container {
+  height: 52px;
+  padding: 12px;
+  background: #ea554126;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+  cursor: pointer;
+  border-radius: 8px;
+}
+.wattage-item {
+  height: 30px;
+  font-size: 26px;
+  scroll-snap-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="home-page">
+    <div class="page-header">
+      <img :src="require('@/assets/images/microwave.svg')" />
+    </div>
+
+    <WattageSelect />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import WattageSelect from "@/components/atoms/WattageSelect.vue";
 
 export default defineComponent({
   name: "Home",
-  components: {},
+  components: {
+    WattageSelect,
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.page-header {
+  display: flex;
+  justify-content: center;
+  padding: 56px;
+}
+</style>
